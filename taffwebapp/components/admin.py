153857b972c5d9b2a782e7d96f90e05fe3e1a46c
woastel <seb-schilling@gmx.de>
2017-05-15 19:55:22 +0200

    something

--- taffwebapp/components/admin.py
+++ taffwebapp/components/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 from .models import (
-    Component,
-    Component_Type,
     Vendor,
-    PCIeCtrl
-
-
+    Component_Type,
+    Component,
+    Component_Character_Thermal,
+    Component_Character_Mechanical,
+    Component_Character_Electrical_Power,
+    Chassis,
+    ChassisAddOn,
+    Motherboard,
+    Cpu,
+    Memory,
+    PSU,
+    HDD,
+    HeatSink,
+    Fan,
+    Cable,
+    Pcba,
+    Pcie_Ctrl
 )
 # Register your models here.
 
@@ -13,4 +25,18 @@ from .models import (
 admin.site.register(Vendor)
 admin.site.register(Component_Type)
 admin.site.register(Component)
-admin.site.register(PCIeCtrl)
+admin.site.register(Component_Character_Mechanical)
+admin.site.register(Component_Character_Electrical_Power)
+admin.site.register(Component_Character_Thermal)
+admin.site.register(Chassis)
+admin.site.register(ChassisAddOn)
+admin.site.register(Motherboard)
+admin.site.register(Cpu)
+admin.site.register(Memory)
+admin.site.register(PSU)
+admin.site.register(HDD)
+admin.site.register(HeatSink)
+admin.site.register(Fan)
+admin.site.register(Cable)
+admin.site.register(Pcba)
+admin.site.register(Pcie_Ctrl)
