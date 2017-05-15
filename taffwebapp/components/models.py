2bc13bceabbb1c472438decf0d0a05d5f7fdb36f
woastel <seb-schilling@gmx.de>
2017-05-15 19:05:36 +0200

    something

--- taffwebapp/components/models.py
+++ taffwebapp/components/models.py
@@ -16,6 +16,9 @@ class Component_Type(models.Model):
         return(str(self.name))
 
 class Component(models.Model):
+    component_id = models.AutoField(primary_key=True)
+
+
     name = models.CharField(max_length=100)
     component_type = models.ForeignKey(Component_Type)
     vendor = models.ForeignKey(Vendor, models.SET_NULL, null=True)
@@ -26,18 +29,26 @@ class Component(models.Model):
     user_updater= models.ForeignKey(User, related_name='%(class)s_requests_created')
     information = models.TextField(max_length=5000)
 
-
     def __str__(self):
         return(str(self.component_type.name) + " - " + str(self.name))
 
-class ComponentMechanicalCharackter(Component):
-    length = models.IntegerField()
-    width = models.IntegerField()
-    high = models.IntegerField()
+class Component_ThermalCharacter(models.Model):
+    thermalCharacter_id = models.AutoField(primary_key=True)
+    maxtemp = models.IntegerField()
 
-class ComponentThermalChararckter(ComponentMechanicalCharackter):
-    maxthermperature = models.IntegerField()
-    airflowminimum = models.IntegerField()
+class Component_MechanicalCharacter(models.Model):
+    material = models.CharField()
+    size_x = models.DecimalField(max_digits=10, decimal_places=2)
+    size_y = models.DecimalField(max_digits=10, decimal_places=2)
+    size_z = models.DecimalField(max_digits=10, decimal_places=2)
 
-class PCIeCtrl(ComponentThermalChararckter):
+class Component_ElectricalCharacter(models.Model):
+    power = models.DecimalField(max_digits=10, decimal_places=2)
+
+# Component specifit characters
+
+
+
+# Die eigentlichen Components
+class PCIeCtrl(Component, Component_ThermalCharacter):
     test = models.IntegerField()
