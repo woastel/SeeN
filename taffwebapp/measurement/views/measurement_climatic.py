3d38b17a6a0561aef627ecaca8aaa4de955c1f8f
woastel <seb-schilling@gmx.de>
2017-05-21 19:06:07 +0200

    new measurement object definition done

--- taffwebapp/measurement/views/measurement_climatic.py
+++ taffwebapp/measurement/views/measurement_climatic.py
@@ -1,4 +1,4 @@
-from .climaticmeasurement import *
+
 from django.views import generic
 from django.shortcuts import render
 
--- taffwebapp/measurement/views/measurement_emi.py
+++ taffwebapp/measurement/views/measurement_emi.py
@@ -1,10 +1,9 @@
-
 from django.views import generic
 from django.shortcuts import render
 
 
 class IndexView(generic.View):
-    template_name = 'measurement/index_.html'
+    template_name = 'measurement/index_measurement_emi.html'
 
     def get(self, request, *args, **kwargs):
 
